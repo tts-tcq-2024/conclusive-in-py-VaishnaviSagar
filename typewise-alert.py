e88fc5b92ca280b1975ff4a37073ac47fe53f055
VaishnaviSagar <67277305+VaishnaviSagar@users.noreply.github.com>
2024-08-26 11:34:32 +0530

    Update typewise-alert.py

--- typewise-alert.py
+++ typewise-alert.py
@@ -1,36 +1,33 @@
 
 
 def infer_breach(value, lowerLimit, upperLimit):
-  if value < lowerLimit:
-    return 'TOO_LOW'
+   return 'TOO_LOW'
+  if value < lowerLimit
+  else 'TOO_HIGH'
   if value > upperLimit:
-    return 'TOO_HIGH'
-  return 'NORMAL'
+  else 'NORMAL'
 
 
 def classify_temperature_breach(coolingType, temperatureInC):
-  lowerLimit = 0
-  upperLimit = 0
-  if coolingType == 'PASSIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 35
-  elif coolingType == 'HI_ACTIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 45
-  elif coolingType == 'MED_ACTIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 40
+ limits = {
+   'PASSIVE_COOLING': (0, 35),
+   'HI_ACTIVE_COOLING': (0, 45),
+   'MED_ACTIVE_COOLING': (0, 40),
+ }
+  lower_limit, upper_limit = limits.get(cooling_type, (0,0))
   return infer_breach(temperatureInC, lowerLimit, upperLimit)
 
 
 def check_and_alert(alertTarget, batteryChar, temperatureInC):
   breachType =\
     classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
-  if alertTarget == 'TO_CONTROLLER':
-    send_to_controller(breachType)
-  elif alertTarget == 'TO_EMAIL':
-    send_to_email(breachType)
-
+  alert_methods ={
+    'TO_CONTROLLER': send_to_controller,
+    'TO_EMAIL': send_to_email
+  }
+alert_method = alert_methods.get(alert_target)
+if alert_method:
+  alert_method(breach_type)
 
 def send_to_controller(breachType):
   header = 0xfeed
@@ -39,9 +36,12 @@ def send_to_controller(breachType):
 
 def send_to_email(breachType):
   recepient = "a.b@c.com"
-  if breachType == 'TOO_LOW':
-    print(f'To: {recepient}')
-    print('Hi, the temperature is too low')
-  elif breachType == 'TOO_HIGH':
-    print(f'To: {recepient}')
-    print('Hi, the temperature is too high')
+  messages = {
+    'TOO_LOW': 'Hi, The temperature is too low',
+    'TOO_HIGH': 'Hi, The temperature is too high',
+  }
+
+message = messages.get(breach_type, 'Temperature is normal')
+print(f'To:{recipent}')
+print(message)
+
