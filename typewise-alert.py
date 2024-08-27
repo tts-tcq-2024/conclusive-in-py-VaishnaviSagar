2229af06c588416e30519507a41e5d86c7f70acd
VaishnaviSagar <67277305+VaishnaviSagar@users.noreply.github.com>
2024-08-27 08:52:32 +0530

    Update typewise-alert.py

--- typewise-alert.py
+++ typewise-alert.py
@@ -1,47 +1,42 @@
+def infer_breach(value, lower_limit, upper_limit):
+    return 'TOO_LOW' if value < lower_limit else 'TOO_HIGH' if value > upper_limit else 'NORMAL'
 
 
-def infer_breach(value, lowerLimit, upperLimit):
-   return 'TOO_LOW'
-if value < lowerLimit:
-   return 'TOO_HIGH'
-if value > upperLimit:
-   return 'NORMAL'
+def classify_temperature_breach(cooling_type, temperature_in_c):
+    limits = {
+        'PASSIVE_COOLING': (0, 35),
+        'HI_ACTIVE_COOLING': (0, 45),
+        'MED_ACTIVE_COOLING': (0, 40)
+    }
+    
+    lower_limit, upper_limit = limits.get(cooling_type, (0, 0))
+    return infer_breach(temperature_in_c, lower_limit, upper_limit)
 
 
-def classify_temperature_breach(coolingType, temperatureInC):
- limits = {
-   'PASSIVE_COOLING': (0, 35),
-   'HI_ACTIVE_COOLING': (0, 45),
-   'MED_ACTIVE_COOLING': (0, 40),
- }
-
-lowerLimit, upperLimit = limits.get(coolingType, (0,0))
-return infer_breach(temperatureInC, lowerLimit, upperLimit)
-
-def check_and_alert(alertTarget, batteryChar, temperatureInC):
-  breachType =\
-    classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
-  alert_methods ={
-    'TO_CONTROLLER': send_to_controller,
-    'TO_EMAIL': send_to_email
-  }
-alert_method = alert_methods.get(alert_target)
-if alert_method:
-  alert_method(breach_type)
-
-def send_to_controller(breachType):
-  header = 0xfeed
-  print(f'{header}, {breachType}')
+def check_and_alert(alert_target, battery_char, temperature_in_c):
+    breach_type = classify_temperature_breach(battery_char['coolingType'], temperature_in_c)
+    alert_methods = {
+        'TO_CONTROLLER': send_to_controller,
+        'TO_EMAIL': send_to_email
+    }
+    
+    alert_method = alert_methods.get(alert_target)
+    if alert_method:
+        alert_method(breach_type)
 
 
-def send_to_email(breachType):
-  recepient = "a.b@c.com"
-  messages = {
-    'TOO_LOW': 'Hi, The temperature is too low',
-    'TOO_HIGH': 'Hi, The temperature is too high',
-  }
+def send_to_controller(breach_type):
+    header = 0xfeed
+    print(f'{header}, {breach_type}')
 
-message = messages.get(breach_type, 'Temperature is normal')
-print(f'To:{recipent}')
-print(message)
 
+def send_to_email(breach_type):
+    recipient = "a.b@c.com"
+    messages = {
+        'TOO_LOW': 'Hi, the temperature is too low',
+        'TOO_HIGH': 'Hi, the temperature is too high'
+    }
+    
+    message = messages.get(breach_type, 'Temperature is normal')
+    print(f'To: {recipient}')
+    print(message)
