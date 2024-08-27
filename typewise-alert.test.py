e324b05a3fa58cce2e56e7727b73ce803c241764
VaishnaviSagar <67277305+VaishnaviSagar@users.noreply.github.com>
2024-08-27 08:33:01 +0530

    Update typewise-alert.test.py

--- typewise-alert.test.py
+++ typewise-alert.test.py
@@ -9,8 +9,33 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(typewise_alert.infer_breach(-5, 0, 35) == 'TOO_LOW')
     self.assertTrue(typewise_alert.infer_breach(40, 0, 35) == 'TOO_HIGH')
 
-def test_classify_temperature_breach(self):
-  self.assertEqual(classify_temperature_breach('PASSIVE_COOLING')
+  def test_classify_temperature_breach(self):
+    self.assertEqual(classify_temperature_breach('PASSIVE_COOLING',25),'NORMAL')
+    self.assertEqual(classify_temperature_breach('HI_ACTIVE_COOLING',-5),'TOO_LOW')
+    self.assertEqual(classify_temperature_breach('MID_ACTIVE_COOLING',50),'TOO_HIGH')
+@patch('sys.stdout', new_callable=StringIO)
+    def test_send_to_controller(self, mock_stdout):
+        send_to_controller('TOO_LOW')
+        self.assertEqual(mock_stdout.getvalue(),expected_output)
+
+@patch('sys.stdout', new_callable=StringIO)
+    def test_send_to_email(self, mock_stdout):
+        send_to_email('TOO_HIGH')
+        expected_output = "To:a.b@cy.com\nHi, The temperature is too high"
+        self.assertEqual(mock_stdout.getvalue(),expected_output)
+
+def test_check_and_alert(self):
+  battery_charge = {'coolingType':'PASSIVE_COOLING'}
+  with patch('__main__.send_to_controller') as mock_send_to_controller:
+    check_and_alert('TO_CONTROLLER', battery_charge, 25)
+    mock_send_to_controller.assert_called_with('NORMAL')
+
+ with patch('__main__.send_to_email') as mock_send_to_email:
+    check_and_alert('TO_EMAIL', battery_charge, -5)
+    mock_send_to_controller.assert_called_with('TOO_LOW')
+
+check_and_alert('INVALID_TARGET', battery_charge, 50)
+
 
 
 if __name__ == '__main__':
