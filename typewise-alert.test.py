89513092364e5697194ba165a52b3a6989d13594
VaishnaviSagar <67277305+VaishnaviSagar@users.noreply.github.com>
2024-08-27 15:54:29 +0530

    Update typewise-alert.test.py

--- typewise-alert.test.py
+++ typewise-alert.test.py
@@ -28,9 +28,9 @@ class TypewiseTest(unittest.TestCase):
 	  self.assertEqual(mock_stdout.getvalue(),expected_output)
 
 	def test_check_and_alert(self):
-	with patch('__main__.send_to_controller') as mock_send_to_controller:
-	typewise_alert.check_and_alert('TO_CONTROLLER', {'coolingType':'PASSIVE_COOLING'}, 25)
-	mock_send_to_controller.assert_called_with('NORMAL')
+		with patch('__main__.send_to_controller') as mock_send_to_controller:
+			typewise_alert.check_and_alert('TO_CONTROLLER', {'coolingType':'PASSIVE_COOLING'}, 25)
+			mock_send_to_controller.assert_called_with('NORMAL')
 
 	with patch('__main__.send_to_email') as mock_send_to_email:
 	typewise_alert.check_and_alert('TO_EMAIL', {'coolingType':'PASSIVE_COOLING'}, -5)
